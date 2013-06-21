c9da3642934a77516773dedb9a1fbaec32ffdc63
danieljfarrell <danieljfarrel@me.com>
2013-06-21 16:16:05 +0900

    First commit of Python code.

---
+++ fvm.py
@@ -0,0 +1,98 @@
+from __future__ import division
+import numpy as np
+import pylab
+from scipy.sparse import linalg
+
+faces = np.linspace(-1,1,300)   # Position of cell faces
+cells = 0.5 * (faces[0:-1] + faces[1:]) # Position of cell centroids
+J = len(cells)                  # Total number of cells
+a = 0.1*np.ones(len(cells))     # Advection velocity
+d = 0.01*np.ones(len(cells))    # Diffusion coefficient
+k = 0.0001                      # Time step 
+
+
+# Define spacing between cell faces and centroids
+h = faces[1:] - faces[0:-1]
+hm = lambda i: cells[i] - cells[i-1] # We can use numpy array arguments
+hp = lambda i: cells[i+1] - cells[i] # here without issue (vectorisable!)
+
+
+# Velocity at cell's left and right hand faces, linear interpolation
+am = lambda i: h[i]/(2*hm(i))*a[i-1] + h[i-1]/(2*hm(i))*a[i]
+ap = lambda i: h[i+1]/(2*hp(i))*a[i] + h[i]/(2*hp(i))*a[i+1]
+
+
+# Diffusion coefficient at cell's left and right hand faces, cell averages
+dm = lambda i: h[i]/(2*hm(i))*d[i-1] + h[i-1]/(2*hm(i))*d[i]
+dp = lambda i: h[i+1]/(2*hp(i))*d[i] + h[i]/(2*hp(i))*d[i+1]
+
+
+# Interior coefficients for matrix equation
+ra = lambda i: k/h[i]*(am(i)*h[i]/(2*hm(i)) - dm(i)/hm(i))
+rb = lambda i: k/h[i]*(am(i)*h[i-1]/(2*hm(i)) - ap(i)*h[i+1]/(2*hp(i)) + dm(i)/hm(i) + dp(i)/hp(i))
+rc = lambda i: k/h[i]*(-ap(i)*h[i]/(2*hp(i)) - dp(i)/hp(i))
+
+
+# Left hand side Robin boundary coefficients for matrix equation
+alphab = k/h[0]*( -ap(0)*h[1]/(2*hp(0)) + dp(0)/hp(0) )
+alphac = k/h[0]*( -ap(0)*h[0]/(2*hp(0)) - dp(0)/hp(0) )
+
+
+# Right hand side Robin boundary coefficients for matrix equation
+betaa = k/h[-1]*( am(-1)*h[-1]/(2*hm(-1)) - dm(-1)/hm(-1) )
+betab = k/h[-1]*( am(-1)*h[-2]/(2*hm(-1)) + dm(-1)/hm(-1) )
+
+
+# Now write in matrix from, theta-method has been applied
+
+# Left hand side matrix
+from scipy import sparse
+theta = 0.5
+padding = np.array([0])
+inx = np.array(range(1,J-1))
+central = np.concatenate([np.array([1-theta*alphab]), 1-theta*rb(inx), np.array([1-theta*betab])])
+inx = np.array(range(1,J-1))
+lower = np.concatenate([-theta*ra(inx), np.array([-theta*betaa]), padding])
+inx = np.array(range(1,J-1))
+upper = np.concatenate([padding, np.array([-theta*alphac]), -theta*rc(inx)])
+diags = [-1,0,1]
+A = sparse.spdiags([lower, central, upper], diags, J, J)
+
+
+# Right hand side matrix 
+padding = np.array([0])
+inx = np.array(range(1,J-1))
+central = np.concatenate([np.array([1+(1-theta)*alphab]), 1+(1-theta)*rb(inx), np.array([1+(1-theta)*betab])])
+inx = np.array(range(1,J-1))
+lower = np.concatenate([(1-theta)*ra(inx), np.array([(1-theta)*betaa]), padding])
+inx = np.array(range(1,J-1))
+upper = np.concatenate([padding, np.array([(1-theta)*alphac]), (1-theta)*rc(inx)])
+diags = [-1,0,1]
+M = sparse.spdiags([lower, central, upper], diags, J, J)
+
+
+# Check Peclet number and CFL condition
+mu = a * h / d
+CFL = a * k / h
+print "Peclet number", np.average(mu)
+print "CFL condition", np.average(CFL)
+
+
+# Initial conditions
+gaussian = lambda z, height, position, hwhm: height * np.exp(-np.log(2) * ((z - position)/hwhm)**2)
+w_init = gaussian(cells, 1, 0, 0.1)
+
+
+# Time sweep
+w = w_init
+for i in range(600):
+    w = linalg.spsolve(A.tocsc(), M * w)
+    #w = linalg.gmres(A.tocsc(), M*w)[0]
+    #w = linalg.lgmres(A.tocsc(), M*w)[0]
+    #w = linalg.minres(A.tocsc(), M*w)[0]
+    if i % 200 == 0:
+        pylab.plot(cells, w)
+        pylab.draw()
+
+pylab.show()
+

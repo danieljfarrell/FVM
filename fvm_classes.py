3ed6be3121eecb5df25622d6829c964836c7a4ca
danieljfarrell <danieljfarrel@me.com>
2013-07-02 09:26:51 +0900

    Syncing up the source code in this branch with master.

--- fvm_classes.py
+++ fvm_classes.py
@@ -1,7 +1,6 @@
 from __future__ import division
 import collections
 import numpy as np
-#import pylab
 from scipy import sparse
 from scipy.sparse import linalg
 np.random.seed(seed=1)
@@ -89,8 +88,8 @@ class Model(object):
         super(Model, self).__init__()
         
         self.mesh = Mesh(faces)
-        self.a = CellVariable(a, mesh=mesh)
-        self.d = CellVariable(d, mesh=mesh)
+        self.a = CellVariable(a, mesh=self.mesh)
+        self.d = CellVariable(d, mesh=self.mesh)
         self.k = k
         self.theta = theta
         self.discretisation = discretisation
@@ -116,11 +115,11 @@ class Model(object):
             self.kappa[np.where(np.isposinf(mu))] = 1
             self.kappa[np.where(np.isneginf(mu))] = -1
         elif discretisation == "upwind":
-            self.kappa = np.zeros(mesh.J)
-            self.kappa[np.where(a>0)] = 1
-            self.kappa[np.where(a<0)] = -1
+            kappa_neg = np.where(self.a<0,-1,0)
+            kappa_pos = np.where(self.a>0,1,0)
+            self.kappa = kappa_neg + kappa_pos
         elif discretisation == "central":
-            self.kappa = np.zeros(mesh.J)
+            self.kappa = np.zeros(self.mesh.J)
         else:
             print "Please set `discretisation` to one of the following: `upwind`, `central` or `exponential`."
         
@@ -141,13 +140,13 @@ class Model(object):
         self.left_value = left_value
         self.right_value = right_value
         
-    def _interior_functions(self):
+    def _interior_elements(self, i):
         
         # Interior coefficients for matrix equation
         ra = lambda i, a, d, m, k: k/m.h(i)*(a.m(i)*m.h(i)/(2*m.hm(i)) + d.m(i)/m.hm(i))
         rb = lambda i, a, d, m, k: k/m.h(i)*(a.m(i)*m.h(i-1)/(2*m.hm(i)) - a.p(i)*m.h(i+1)/(2*m.hp(i)) - d.m(i)/m.hm(i) - d.p(i)/m.hp(i))
         rc = lambda i, a, d, m, k: k/m.h(i)*(-a.p(i)*m.h(i)/(2*m.hp(i)) + d.p(i)/m.hp(i))
-        return ra, rb, rc
+        return ra(i, self.a, self.d, self.mesh, self.k), rb(i, self.a, self.d, self.mesh, self.k), rc(i,self.a, self.d, self.mesh, self.k)
         
     def _robin_boundary_condition_elements_left(self, matrix=None):
         
@@ -224,17 +223,17 @@ class Model(object):
     
     def _dirichlet_boundary_condition_vector_elements_left(self):
         # Vector elements for Dirichlet boundary condition
-        ra, rb, rc = self._interior_functions()
+        ra, rb, rc = self._interior_elements(1)
         locations = [0,1]
-        values = [self.left_value, ra(1, self.a, self.d, self.mesh, self.k)*self.left_value]
+        values = [self.left_value, ra*self.left_value]
         return tuple([list(x) for x in zip(locations, values)])
         
     def _dirichlet_boundary_condition_vector_elements_right(self):
         # Vector elements for Dirichlet boundary condition
-        ra, rb, rc = self._interior_functions()
         J = self.mesh.J
+        ra, rb, rc = self._interior_elements(J-2)
         locations = [J-2, J-1]
-        values = [rc(J-2,self.a, self.d, self.mesh, self.k)*self.right_value, self.right_value]
+        values = [rc*self.right_value, self.right_value]
         return tuple([list(x) for x in zip(locations, values)])
         
     def A_matrix(self):
@@ -250,8 +249,6 @@ class Model(object):
         zero = padding          # Yes, its the same. But this element is included in the matrix (semantic difference).
         one  = np.array([1])    #
         
-        ra, rb, rc = self._interior_functions()
-        
         if self.left_flux is not None:
             left_bc_elements = self._robin_boundary_condition_elements_left(matrix="A")
         
@@ -266,16 +263,15 @@ class Model(object):
         
         # Use the functions to layout the matrix
         inx = np.array(range(1,J-1))
+        ra, rb, rc = self._interior_elements(inx)
         #                                 c1
-        upper = np.concatenate([padding, zero, -t*rc(inx, a, d, mesh, k) ]) 
+        upper = np.concatenate([padding, zero, -t*rc ]) 
         
-        inx = np.array(range(1,J-1))
-        #                          b1                               bJ
-        central = np.concatenate([zero, 1-t*rb(inx, a, d, mesh, k), zero  ]) 
+        #                          b1           bJ
+        central = np.concatenate([zero, 1-t*rb, zero  ]) 
         
-        inx = np.array(range(1,J-1))
-        #                                                  aJ
-        lower = np.concatenate([-t*ra(inx, a, d, mesh, k), zero , padding])
+        #                               aJ
+        lower = np.concatenate([-t*ra, zero , padding])
         
         A = sparse.spdiags([lower, central, upper], [-1,0,1], J, J).todok()
         
@@ -298,8 +294,6 @@ class Model(object):
         zero = padding          # Yes, its the same. But this element is included in the matrix (semantic difference).
         one  = np.array([1])    #
         
-        ra, rb, rc = self._interior_functions()
-        
         if self.left_flux is not None:
             left_bc_elements = self._robin_boundary_condition_elements_left(matrix="M")
         
@@ -314,16 +308,15 @@ class Model(object):
         
         # Use the functions to layout the matrix
         inx = np.array(range(1,J-1))
+        ra, rb, rc = self._interior_elements(inx)
         #                                 c1
-        upper = np.concatenate([padding, zero, (1-t)*rc(inx,a, d, mesh, k) ])
+        upper = np.concatenate([padding, zero, (1-t)*rc ])
         
-        inx = np.array(range(1,J-1))
-        #                          b1                                  bJ
-        central = np.concatenate([zero, 1+(1-t)*rb(inx,a, d, mesh, k), zero  ]) 
+        #                          b1               bJ
+        central = np.concatenate([zero, 1+(1-t)*rb, zero  ]) 
         
-        inx = np.array(range(1,J-1))
-        #                                                     aJ
-        lower = np.concatenate([(1-t)*ra(inx,a, d, mesh, k), zero , padding])
+        #                                   aJ
+        lower = np.concatenate([(1-t)*ra, zero , padding])
         
         A = sparse.spdiags([lower, central, upper], [-1,0,1], J, J).todok()
         
@@ -353,71 +346,73 @@ class Model(object):
             b[inx] = value
         return b
 
-#faces = np.concatenate((np.array([-0.5]), np.sort(np.random.uniform(-0.5, 1, 50)), np.array([1])))
-faces = np.linspace(0, 1, 50)
-mesh =  Mesh(faces)
+if __name__ == '__main__':
 
-a = CellVariable(1, mesh=mesh) # Advection velocity
-d = CellVariable(1e-3, mesh=mesh) # Diffusion coefficient
-k = 0.01                             # Time step 
-
-model = Model(faces, a, d, k, discretisation="exponential")
-model.set_boundary_conditions(left_value=1., right_value=0.)
-#model.set_boundary_conditions(left_flux=0, right_flux=0)
-#model.set_boundary_conditions(left_value=1, right_flux=0)
-A = model.A_matrix()
-M = model.M_matrix()
-b = model.b_vector()
-
-print "Peclet number", np.min(model.peclet_number()), np.max(model.peclet_number())
-print "CFL condition", np.min(model.CFL_condition()), np.max(model.CFL_condition())
-
-# Initial conditions
-w_init = 0.5*TH(mesh.cells, 0.4, 0)
-w_init = np.sin(np.pi*mesh.cells)**100
-
-# Source term
-b[int(np.median(range(mesh.J)))] = 0.0
-
-# matplotlib for movie export
-# see, http://matplotlib.org/examples/animation/moviewriter.html
-import matplotlib
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
-import matplotlib.animation as manimation
-print manimation.writers.__dict__
-FFMpegWriter = manimation.writers['ffmpeg']
-metadata = dict(title='Movie Test', artist='Matplotlib', comment='Movie support!')
-writer = FFMpegWriter(fps=15, metadata=metadata)
-
-fig = plt.figure()
-l0, = plt.plot([],[], 'r-', lw=1)
-l1, = plt.plot([],[], 'k-o', markersize=4)
-plt.xlim(np.min(faces), np.max(faces))
-plt.ylim(-0.2,1.2)
-l1.set_data(mesh.cells,w_init)
-
-# # Analytical solution for Dirichlet boundary conditions
-analytical_x = np.concatenate([np.array([np.min(faces)]), mesh.cells, np.array([np.max(faces)])])
-analytical_solution = np.concatenate([np.array([model.left_value]), (np.exp(a/d) - np.exp(mesh.cells*a/d))/(np.exp(a/d)-1), np.array([model.right_value]) ])
-#analytical_solution2 = np.concatenate([np.array([model.left_value]), (np.exp(a/model.d) - np.exp(mesh.cells*a/model.d))/(np.exp(a/model.d)-1), np.array([model.right_value]) ])
-
-w = w_init
-with writer.saving(fig, "writer_test.mp4", 300):
+    #faces = np.concatenate((np.array([-0.5]), np.sort(np.random.uniform(-0.5, 1, 50)), np.array([1])))
+    faces = np.linspace(0, 1, 50)
+    mesh =  Mesh(faces)
     
-    for i in range(201):
-        w = linalg.spsolve(A.tocsc(), M * w + b)
+    a = CellVariable(1, mesh=mesh) # Advection velocity
+    d = CellVariable(1e-3, mesh=mesh) # Diffusion coefficient
+    k = 0.01                             # Time step 
+    
+    model = Model(faces, a, d, k, discretisation="exponential")
+    model.set_boundary_conditions(left_value=1., right_value=0.)
+    #model.set_boundary_conditions(left_flux=0, right_flux=0)
+    #model.set_boundary_conditions(left_value=1, right_flux=0)
+    A = model.A_matrix()
+    M = model.M_matrix()
+    b = model.b_vector()
+    
+    print "Peclet number", np.min(model.peclet_number()), np.max(model.peclet_number())
+    print "CFL condition", np.min(model.CFL_condition()), np.max(model.CFL_condition())
+    
+    # Initial conditions
+    w_init = 0.5*TH(mesh.cells, 0.4, 0)
+    w_init = np.sin(np.pi*mesh.cells)**100
+    
+    # Source term
+    b[int(np.median(range(mesh.J)))] = 0.0
+    
+    # matplotlib for movie export
+    # see, http://matplotlib.org/examples/animation/moviewriter.html
+    import matplotlib
+    matplotlib.use("Agg")
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as manimation
+    print manimation.writers.__dict__
+    FFMpegWriter = manimation.writers['ffmpeg']
+    metadata = dict(title='Movie Test', artist='Matplotlib', comment='Movie support!')
+    writer = FFMpegWriter(fps=15, metadata=metadata)
+    
+    fig = plt.figure()
+    l0, = plt.plot([],[], 'r-', lw=1)
+    l1, = plt.plot([],[], 'k-o', markersize=4)
+    plt.xlim(np.min(faces), np.max(faces))
+    plt.ylim(-0.2,1.2)
+    l1.set_data(mesh.cells,w_init)
+    
+    # # Analytical solution for Dirichlet boundary conditions
+    analytical_x = np.concatenate([np.array([np.min(faces)]), mesh.cells, np.array([np.max(faces)])])
+    analytical_solution = np.concatenate([np.array([model.left_value]), (np.exp(a/d) - np.exp(mesh.cells*a/d))/(np.exp(a/d)-1), np.array([model.right_value]) ])
+    #analytical_solution2 = np.concatenate([np.array([model.left_value]), (np.exp(a/model.d) - np.exp(mesh.cells*a/model.d))/(np.exp(a/model.d)-1), np.array([model.right_value]) ])
+    
+    w = w_init
+    with writer.saving(fig, "writer_test.mp4", 300):
+    
+        for i in range(201):
+            w = linalg.spsolve(A.tocsc(), M * w + b)
         
-        if  i == 0:
-            l1.set_data(mesh.cells,w_init)
-            writer.grab_frame()
+            if  i == 0:
+                l1.set_data(mesh.cells,w_init)
+                writer.grab_frame()
             
-        if i %  1 == 0 or i == 0:
-            l1.set_data(mesh.cells,w)
-            #l0.set_data(analytical_x, analytical_solution)
-            area = np.sum(w * mesh.cell_widths)
-            print "#%d; t=%g; area=%g:" % (i, i*k,area)
-            writer.grab_frame()
+            if i %  1 == 0 or i == 0:
+                l1.set_data(mesh.cells,w)
+                #l0.set_data(analytical_x, analytical_solution)
+                area = np.sum(w * mesh.cell_widths)
+                print "#%d; t=%g; area=%g:" % (i, i*k,area)
+                writer.grab_frame()
 
 
 

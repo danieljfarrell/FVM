f200048730a33aa893c5c8eca965ab978caeca67
danieljfarrell <danieljfarrel@me.com>
2013-08-29 10:59:40 +0900

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "fvm",
+    version = "0.1",
+    author = "Daniel J Farrell",
+    author_email = "daniel.james.farrell@gmail.com",
+    description = ("Examples of solving conservation problems using the finite volume method")
+    license = "BSD",
+    keywords = "finite volume numerical partial differential equations",
+    url = "http://github.com/danieljfarrell/FVM",
+    packages=['fvm'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Physics",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
